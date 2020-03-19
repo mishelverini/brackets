2afc65fed43be117ef394c1795a435a329a05f07
Mikhail Vazhynski <mishelverini@yahoo.com>
2020-03-19 14:32:14 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strRef = str.split('');
+  let conf = bracketsConfig.flat();
+  let hooks = (strRef.length % 2) == 0;
+  let difference = conf.filter(function(elemrnt){
+    return !strRef.includes(elemrnt);
+  });
+  let closeStr  = !(strRef[0] == ')' || strRef[strRef.length - 1] == '(');
+  let hooksDouble = true;
+  if (4 == strRef.length) {
+    if(strRef[0] == '(') {
+      hooksDouble = strRef[3] == ')' || strRef[1] == ')';
+    }
+    if(strRef[0] == '[') {
+      hooksDouble = strRef[3] == ']' || strRef[1] == ']';
+    }
+    if(strRef[0] == '|') {
+      hooksDouble = strRef[3] == '|' || strRef[1] == '|';
+    }
+  }
+  
+  return difference.length == 0 && hooks && closeStr && hooksDouble;
 }
