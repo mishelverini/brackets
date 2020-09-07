5136b4b21f59c2014a0ce2ba3750dc7570c1bd3e
Mishel Verini <mishelverini@yahoo.com>
2020-09-07 22:33:24 +0300

    regactor

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  let strRef = str.split('');
-  let conf = bracketsConfig.flat();
-  let hooks = (strRef.length % 2) == 0;
-  let difference = conf.filter(function(elemrnt){
-    return !strRef.includes(elemrnt);
-  });
-  let closeStr  = !(strRef[0] == ')' || strRef[strRef.length - 1] == '(');
-  let hooksDouble = true;
-  if (4 == strRef.length) {
-    if(strRef[0] == '(') {
-      hooksDouble = strRef[3] == ')' || strRef[1] == ')';
+  const configOpen = [];
+  const configClose = [];
+  let res = [];
+  const strArr = str.split('');
+
+  const isOpen = (value) => configOpen.includes(value);
+  const getClose = (value) => configClose[configOpen.indexOf(value)];
+  const double = (value) => configOpen.includes(value) === configClose.includes(value);
+
+  for ([open, close] of bracketsConfig) {
+    configOpen.push(open);
+    configClose.push(close);
+  }
+
+  for (let s = 0; s < strArr.length; s++) {
+    if (double(strArr[s])) {
+      if (strArr[s] === res[res.length - 1]) {
+        res.pop();
+      } else {
+        res.push(strArr[s]);
+      }
+      continue;
     }
-    if(strRef[0] == '[') {
-      hooksDouble = strRef[3] == ']' || strRef[1] == ']';
-    }
-    if(strRef[0] == '|') {
-      hooksDouble = strRef[3] == '|' || strRef[1] == '|';
+    if (isOpen(strArr[s])) {
+      res.push(getClose(strArr[s]));
+    } else {
+      const last = res.pop();
+      if (strArr[s] !== last) {
+        return false;
+      }
     }
   }
-  
-  return difference.length == 0 && hooks && closeStr && hooksDouble;
+
+  return res.length === 0;
 }
